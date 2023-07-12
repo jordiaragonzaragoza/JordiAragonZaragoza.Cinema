85724adeba3ea0d7c89f3da00b9432ea847fb71b
jordiaragonzaragoza <jordiaragonzaragoza@gmail.com>
2023-07-12 19:32:37 +0200

    Added ShowtimeDeletedNotification.

---
+++ src/JordiAragon.Cinema.Application/Features/Auditorium/Events/ShowtimeDeletedNotificationHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace JordiAragon.Cinema.Application.Features.Auditorium.Events
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Ardalis.GuardClauses;
+    using JordiAragon.Cinema.Application.Contracts.Features.Showtime.Events;
+    using JordiAragon.SharedKernel.Application.Contracts.Interfaces;
+    using Microsoft.Extensions.Logging;
+
+    public class ShowtimeDeletedNotificationHandler : IApplicationEventNotificationHandler<ShowtimeDeletedNotification>
+    {
+        private readonly ILogger<ShowtimeDeletedNotificationHandler> logger;
+
+        public ShowtimeDeletedNotificationHandler(
+            ILogger<ShowtimeDeletedNotificationHandler> logger)
+        {
+            this.logger = Guard.Against.Null(logger, nameof(logger));
+        }
+
+        public Task Handle(ShowtimeDeletedNotification notification, CancellationToken cancellationToken)
+        {
+            this.logger.LogInformation("Handled Notification: {ApplicationEvent}", notification.GetType().Name);
+
+            // TODO: Use some service to notify the admin.
+            return Task.CompletedTask;
+        }
+    }
+}
