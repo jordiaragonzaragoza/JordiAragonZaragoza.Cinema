ad82187afa057798d823122c2bc0cceb10bf0f14
jordiaragonzaragoza <jordiaragonzaragoza@gmail.com>
2024-01-22 17:34:42 +0100

    Added FluentValidation.DependencyInjectionExtensions and BaseValidator from shared kernel.

--- src/JordiAragon.Cinema.Reservation/Features/Showtime/Application/Commands/CreateShowtime/CreateShowtimeCommandValidator.cs
+++ src/JordiAragon.Cinema.Reservation/Features/Showtime/Application/Commands/CreateShowtime/CreateShowtimeCommandValidator.cs
@@ -4,9 +4,10 @@
     using Ardalis.GuardClauses;
     using FluentValidation;
     using JordiAragon.Cinema.Reservation.Showtime.Application.Contracts.Commands;
+    using JordiAragon.SharedKernel.Application.Validators;
     using JordiAragon.SharedKernel.Domain.Contracts.Interfaces;
 
-    public class CreateShowtimeCommandValidator : AbstractValidator<CreateShowtimeCommand>
+    public class CreateShowtimeCommandValidator : BaseValidator<CreateShowtimeCommand>
     {
         private readonly IDateTime dateTime;
 
