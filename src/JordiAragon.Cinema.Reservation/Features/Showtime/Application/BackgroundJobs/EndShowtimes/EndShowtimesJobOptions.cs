4bba57a615140ca610b93c725f3821f09647ff05
jordiaragonzaragoza <jordiaragonzaragoza@gmail.com>
2024-04-26 20:00:49 +0200

    Remove batch job to End Showtimes will be implemented on sagas with timeouts.

--- src/JordiAragon.Cinema.Reservation/Features/Showtime/Application/BackgroundJobs/EndShowtimes/EndShowtimesJobOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JordiAragon.Cinema.Reservation.Showtime.Application.BackgroundJobs.EndShowtimes
-{
-    public sealed class EndShowtimesJobOptions
-    {
-        public const string Section = "BackgroundJobs:EndShowtimesJob";
-
-        public int ScheduleIntervalInSeconds { get; set; }
-    }
-}
