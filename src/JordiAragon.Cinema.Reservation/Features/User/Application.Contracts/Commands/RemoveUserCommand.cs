1770856c11ab2ec9ed3d1ce8fb1c759eea5c1bea
jordiaragonzaragoza <jordiaragonzaragoza@gmail.com>
2024-07-04 18:31:50 +0200

    Added Use Cases: CreateUser and RemoveUser.

---
+++ src/JordiAragon.Cinema.Reservation/Features/User/Application.Contracts/Commands/RemoveUserCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace JordiAragon.Cinema.Reservation.User.Application.Contracts.Commands
+{
+    using System;
+    using JordiAragon.SharedKernel.Application.Contracts.Interfaces;
+
+    public sealed record class RemoveUserCommand(Guid UserId) : ICommand;
+}
---
+++ src/JordiAragon.Cinema.Reservation/Features/User/Application/CommandHanders/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace JordiAragon.Cinema.Reservation.User.Application.CommandHanders.CreateUser
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Ardalis.GuardClauses;
+    using Ardalis.Result;
+    using JordiAragon.Cinema.Reservation.User.Application.Contracts.Commands;
+    using JordiAragon.Cinema.Reservation.User.Domain;
+    using JordiAragon.SharedKernel.Application.Commands;
+    using JordiAragon.SharedKernel.Contracts.Repositories;
+
+    public sealed class CreateUserCommandHandler : BaseCommandHandler<CreateUserCommand>
+    {
+        private readonly IRepository<User, UserId> userRepository;
+
+        public CreateUserCommandHandler(IRepository<User, UserId> userRepository)
+        {
+            this.userRepository = Guard.Against.Null(userRepository, nameof(userRepository));
+        }
+
+        public override async Task<Result> Handle(CreateUserCommand command, CancellationToken cancellationToken)
+        {
+            var newUser = User.Create(
+                id: UserId.Create(command.UserId));
+
+            await this.userRepository.AddAsync(newUser, cancellationToken);
+
+            return Result.Success();
+        }
+    }
+}
