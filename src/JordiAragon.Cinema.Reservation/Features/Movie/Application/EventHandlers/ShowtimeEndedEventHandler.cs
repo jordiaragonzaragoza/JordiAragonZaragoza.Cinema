f838e76dd275c1395f32e67a5e6cdae9dd0b8319
jordiaragonzaragoza <jordiaragonzaragoza@gmail.com>
2024-07-12 20:15:18 +0200

    ShowtimeEndedEventHandler to update active showtimes on Movie aggregate.

---
+++ src/JordiAragon.Cinema.Reservation/Features/Movie/Application/EventHandlers/ShowtimeEndedEventHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace JordiAragon.Cinema.Reservation.Movie.Application.EventHandlers
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Ardalis.GuardClauses;
+    using JordiAragon.Cinema.Reservation.Movie.Domain;
+    using JordiAragon.Cinema.Reservation.Showtime.Domain;
+    using JordiAragon.Cinema.Reservation.Showtime.Domain.Events;
+    using JordiAragon.SharedKernel.Application.Contracts.Interfaces;
+    using JordiAragon.SharedKernel.Contracts.Repositories;
+
+    using NotFoundException = JordiAragon.SharedKernel.Domain.Exceptions.NotFoundException;
+
+    public sealed class ShowtimeEndedEventHandler : IEventHandler<ShowtimeEndedEvent>
+    {
+        private readonly IRepository<Movie, MovieId> movieRepository;
+
+        public ShowtimeEndedEventHandler(
+            IRepository<Movie, MovieId> movieRepository)
+        {
+            this.movieRepository = Guard.Against.Null(movieRepository, nameof(movieRepository));
+        }
+
+        public async Task Handle(ShowtimeEndedEvent @event, CancellationToken cancellationToken)
+        {
+            var existingMovie = await this.movieRepository.GetByIdAsync(MovieId.Create(@event.MovieId), cancellationToken)
+                                    ?? throw new NotFoundException(nameof(Movie), @event.MovieId.ToString());
+
+            existingMovie.RemoveActiveShowtime(ShowtimeId.Create(@event.AggregateId));
+
+            await this.movieRepository.UpdateAsync(existingMovie, cancellationToken);
+        }
+    }
+}
