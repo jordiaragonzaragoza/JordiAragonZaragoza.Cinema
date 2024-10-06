c37242dcd0a1f5b5a00ff1e364637e9ecf7f9fa1
Jordi Aragon Zaragoza <jordiaragonzaragoza@gmail.com>
2024-10-06 19:27:39 +0200

    Fix Serilog.Sink.Async for Console.

--- src/JordiAragon.Cinema/Program.cs
+++ src/JordiAragon.Cinema/Program.cs
@@ -2,7 +2,7 @@ namespace JordiAragon.Cinema
 {
     using Aspire.Hosting;
 
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S1118:Utility classes should not have public constructors", Justification = "Program class should not have a protected constructor or the static keyword because is used in WebApplicationFactory for functional and integration test.")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S1118:Utility classes should not have public constructors", Justification = "Program class should not have a protected constructor or the static keyword because is used for functional and integration test.")]
     public sealed class Program
     {
         public static void Main(string[] args)
