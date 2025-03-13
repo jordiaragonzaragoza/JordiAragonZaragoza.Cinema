7a15fade0b477ca71db9524f3c0f614530168935
Jordi Aragon Zaragoza <jordiaragonzaragoza@gmail.com>
2025-03-13 17:56:06 +0100

    Added Seats to AuditoriumReadModel

--- src/JordiAragonZaragoza.Cinema.Reservation/Auditorium/Application.Contracts/ReadModels/AuditoriumReadModel.cs
+++ src/JordiAragonZaragoza.Cinema.Reservation/Auditorium/Application.Contracts/ReadModels/AuditoriumReadModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace JordiAragonZaragoza.Cinema.Reservation.Auditorium.Application.Contracts.ReadModels
 {
     using System;
+    using System.Collections.Generic;
     using Ardalis.GuardClauses;
     using JordiAragonZaragoza.SharedKernel.Application.Contracts.Interfaces;
 
@@ -8,10 +9,12 @@
     {
         public AuditoriumReadModel(
             Guid id,
-            string name)
+            string name,
+            IEnumerable<SeatReadModel> seats)
         {
             this.Id = Guard.Against.Default(id, nameof(id));
             this.Name = Guard.Against.Default(name, nameof(name));
+            this.Seats = Guard.Against.NullOrEmpty(seats, nameof(seats));
         }
 
         // Required by EF.
@@ -22,5 +25,7 @@
         public Guid Id { get; private set; }
 
         public string Name { get; private set; } = default!;
+
+        public IEnumerable<SeatReadModel> Seats { get; private set; } = [];
     }
 }
