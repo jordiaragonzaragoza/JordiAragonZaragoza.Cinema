8db6c5a0399f740796f3004e55f5e8fda1c13f99
Jordi Aragon Zaragoza <jordiaragonzaragoza@gmail.com>
2025-04-13 21:19:59 +0200

    Added SystemTests using Aspire Testing.

---
+++ tests/SystemTests/JordiAragonZaragoza.Cinema.SystemTests/Reservation/HttpRestfulApi/V2/Auditorium/GetAuditoriumsTests.cs
@@ -0,0 +1,36 @@
+﻿namespace JordiAragonZaragoza.Cinema.SystemTests.Reservation.HttpRestfulApi.V2.Auditorium
+{
+    using System.Threading.Tasks;
+    using Ardalis.HttpClientTestExtensions;
+    using FluentAssertions;
+    using JordiAragonZaragoza.Cinema.Reservation.Presentation.HttpRestfulApi.Contracts.V2.Auditorium.Requests;
+    using JordiAragonZaragoza.Cinema.Reservation.Presentation.HttpRestfulApi.Contracts.V2.Auditorium.Responses;
+    using JordiAragonZaragoza.Cinema.SystemTests.Common;
+    using JordiAragonZaragoza.SharedKernel.Presentation.HttpRestfulApi.Contracts;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public sealed class GetAuditoriumsTests : BaseSystemTests
+    {
+        public GetAuditoriumsTests(
+            SystemTestsFixture fixture,
+            ITestOutputHelper outputHelper)
+            : base(fixture, outputHelper)
+        {
+        }
+
+        [Fact]
+        public async Task GetAllAuditoriums_WhenHavingValidUrl_ShouldReturnOneAuditorium()
+        {
+            // Arrange
+            var url = $"api/v2/{GetAuditoriumsRequest.Route}";
+
+            // Act
+            var response = await this.Fixture.ReservationHttpClient.GetAndDeserializeAsync<PaginatedCollectionResponse<AuditoriumResponse>>(url, this.OutputHelper);
+
+            // Assert
+            response.Should().NotBeNull();
+            response.Items.Should().HaveCount(1);
+        }
+    }
+}
