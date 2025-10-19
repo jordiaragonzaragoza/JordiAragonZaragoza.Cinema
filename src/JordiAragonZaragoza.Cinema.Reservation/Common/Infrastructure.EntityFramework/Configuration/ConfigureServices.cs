5592365c1bc94578479613693a039ff6338f9e1e
Jordi Aragon Zaragoza <jordiaragonzaragoza@gmail.com>
2025-10-19 21:21:19 +0200

    Remove ProcessOutboxMessagesJob

--- src/JordiAragonZaragoza.Cinema.Reservation/Common/Infrastructure.EntityFramework/Configuration/ConfigureServices.cs
+++ src/JordiAragonZaragoza.Cinema.Reservation/Common/Infrastructure.EntityFramework/Configuration/ConfigureServices.cs
@@ -1,25 +1,16 @@
 ﻿namespace JordiAragonZaragoza.Cinema.Reservation.Common.Infrastructure.EntityFramework.Configuration
 {
-    using JordiAragonZaragoza.Cinema.Reservation.Common.Infrastructure.Outbox;
     using JordiAragonZaragoza.Cinema.SharedKernel;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Diagnostics;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using Quartz;
-    using Volo.Abp.Guids;
 
     public static class ConfigureServices
     {
         public static IServiceCollection AddEntityFrameworkServices(this IServiceCollection serviceCollection, IConfiguration configuration, bool isDevelopment)
         {
-            serviceCollection.Configure<AbpSequentialGuidGeneratorOptions>(options =>
-            {
-                // Recomended option to Generate Guids on PostgreSQL Databases.
-                options.DefaultSequentialGuidType = SequentialGuidType.SequentialAsString;
-            });
-
             serviceCollection.AddDbContext<ReservationBusinessModelContext>(optionsBuilder =>
             {
                 optionsBuilder.UseNpgsql(configuration.GetConnectionString(Constants.JordiAragonZaragozaCinemaReservationBusinessModelStore))
@@ -39,22 +30,6 @@
             ////serviceCollection.AddHealthChecks().AddDbContextCheck<ReservationReadModelContext>();
 
             serviceCollection.AddDatabaseDeveloperPageExceptionFilter();
-
-            serviceCollection.AddQuartz(configure =>
-            {
-                var jobKey = new JobKey(nameof(ProcessOutboxMessagesJob));
-
-                // This Bind is required because AddQuartz dont support IServiceProvider / option pattern.
-                var processOutboxMessagesJobOptions = new ProcessOutboxMessagesJobOptions();
-                configuration.GetSection(ProcessOutboxMessagesJobOptions.Section).Bind(processOutboxMessagesJobOptions);
-
-                var intervalInSeconds = processOutboxMessagesJobOptions.ScheduleIntervalInSeconds;
-
-                configure.AddJob<ProcessOutboxMessagesJob>(jobKey)
-                .AddTrigger(trigger => trigger.ForJob(jobKey)
-                                              .WithSimpleSchedule(schedule => schedule.WithIntervalInSeconds(intervalInSeconds)
-                                                                                      .RepeatForever()));
-            });
 
             return serviceCollection;
         }
