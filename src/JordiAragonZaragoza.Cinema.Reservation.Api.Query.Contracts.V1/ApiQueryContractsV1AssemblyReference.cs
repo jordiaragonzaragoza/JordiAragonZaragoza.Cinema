cb4bcac498e35ce3ef42dc22e078779478b60f68
Jordi Aragon Zaragoza <jordiaragonzaragoza@gmail.com>
2025-11-06 21:21:52 +0100

    Added Api.Query.Contracts

---
+++ src/JordiAragonZaragoza.Cinema.Reservation.Api.Query.Contracts.V1/ApiQueryContractsV1AssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿namespace JordiAragonZaragoza.Cinema.Reservation.Api.Query.Contracts.V1
+{
+    using System.Reflection;
+
+    public static class ApiQueryContractsV1AssemblyReference
+    {
+        public static readonly Assembly Assembly = typeof(ApiQueryContractsV1AssemblyReference).Assembly;
+    }
+}
