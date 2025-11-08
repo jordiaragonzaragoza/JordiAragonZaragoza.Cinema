5ff2650e636cd12cab718b87c3b19508e2a8b63a
Jordi Aragon Zaragoza <jordiaragonzaragoza@gmail.com>
2025-11-08 09:40:44 +0100

    Added Api.Query.Contracts.V1

---
+++ src/JordiAragonZaragoza.Cinema.Reservation.Api.Query.Contracts.V1/Auditorium/AuditoriumRoutes.cs
@@ -0,0 +1,10 @@
+﻿namespace JordiAragonZaragoza.Cinema.Reservation.Api.Query.Contracts.V1.Auditorium
+{
+    public static class AuditoriumRoutes
+    {
+        public const string GetAuditoriums = $"{Base}";
+        public const string GetShowtimes = $"{Base}/{{auditoriumId}}/showtimes";
+        public const string GetAvailableSeats = $"{Base}/{{auditoriumId}}/showtimes/{{showtimeId}}/seats/available";
+        private const string Base = $"{Routes.ApiBase}/auditoriums";
+    }
+}
---
+++ src/JordiAragonZaragoza.Cinema.Reservation.Api.Query.Contracts.V1/Routes.cs
@@ -0,0 +1,7 @@
+﻿namespace JordiAragonZaragoza.Cinema.Reservation.Api.Query.Contracts.V1
+{
+    public static class Routes
+    {
+        public const string ApiBase = "api/v1";
+    }
+}
