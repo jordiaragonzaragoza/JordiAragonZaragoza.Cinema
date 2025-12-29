a96ffc150ed31e8e5cf1f2e2e5e11702dd80eaee
Jordi Aragon Zaragoza <jordiaragonzaragoza@gmail.com>
2025-12-29 19:28:23 +0100

    Change EventStoreDb namespace to KurrentDb

--- src/JordiAragonZaragoza.Cinema.Reservation.Worker.Reactor/Configuration/SeederDependencyInjection.cs
+++ src/JordiAragonZaragoza.Cinema.Reservation.Worker.Reactor/Configuration/SeederDependencyInjection.cs
@@ -2,13 +2,13 @@
 {
     using Microsoft.Extensions.DependencyInjection;
     using JordiAragonZaragoza.SharedKernel.Infrastructure.EventStore;
-    using JordiAragonZaragoza.SharedKernel.Infrastructure.EventStore.EventStoreDb;
+    using JordiAragonZaragoza.SharedKernel.Infrastructure.EventStore.KurrentDb;
 
     public static class SeederDependencyInjection
     {
         public static IServiceCollection AddInfrastructureEventStoreSeeder(this IServiceCollection serviceCollection)
         {
-            serviceCollection.AddScoped<IEventStore, EventStoreDbEventStore>();
+            serviceCollection.AddScoped<IEventStore, KurrentDbEventStore>();
 
             return serviceCollection;
         }
