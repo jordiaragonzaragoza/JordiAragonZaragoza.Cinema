f5191b15d70a3ddbf428379d7d230d0e7bec8aaa
Jordi Aragon Zaragoza <jordiaragonzaragoza@gmail.com>
2026-01-26 18:51:09 +0100

    Rename IntegrationMessages to Integration

---
+++ src/JordiAragonZaragoza.Cinema.Reservation.Application.Contracts.Integration.V1/IntegrationV1AssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿namespace JordiAragonZaragoza.Cinema.Reservation.Application.Contracts.Integration.V1
+{
+    using System.Reflection;
+
+    public static class IntegrationV1AssemblyReference
+    {
+        public static readonly Assembly Assembly = typeof(IntegrationV1AssemblyReference).Assembly;
+    }
+}
