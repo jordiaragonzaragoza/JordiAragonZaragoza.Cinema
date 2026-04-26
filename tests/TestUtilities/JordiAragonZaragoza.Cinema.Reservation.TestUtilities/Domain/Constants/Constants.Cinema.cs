05d4edf06b3e3d224fc7e29af54a36f576ffc2e3
Jordi Aragon Zaragoza <jordiaragonzaragoza@gmail.com>
2026-04-26 12:16:50 +0200

    Add unit tests on User aggregate for authorization

---
+++ tests/TestUtilities/JordiAragonZaragoza.Cinema.Reservation.TestUtilities/Domain/Constants/Constants.Cinema.cs
@@ -0,0 +1,13 @@
+namespace JordiAragonZaragoza.Cinema.Reservation.TestUtilities.Domain
+{
+    using System;
+    using JordiAragonZaragoza.Cinema.Reservation.Cinema.Domain;
+
+    public static partial class Constants
+    {
+        public static class Cinema
+        {
+            public static readonly CinemaId Id = new(new Guid("a1b2c3d4-e5f6-4a5b-6c7d-8e9f0a1b2c3d"));
+        }
+    }
+}
---
+++ tests/TestUtilities/JordiAragonZaragoza.Cinema.Reservation.TestUtilities/Domain/Constants/Constants.Role.cs
@@ -0,0 +1,11 @@
+namespace JordiAragonZaragoza.Cinema.Reservation.TestUtilities.Domain
+{
+    public static partial class Constants
+    {
+        public static class Role
+        {
+            public static readonly global::JordiAragonZaragoza.Cinema.Reservation.User.Domain.Role Admin = global::JordiAragonZaragoza.Cinema.Reservation.User.Domain.Role.Create("Admin");
+            public static readonly global::JordiAragonZaragoza.Cinema.Reservation.User.Domain.Role Viewer = global::JordiAragonZaragoza.Cinema.Reservation.User.Domain.Role.Create("Viewer");
+        }
+    }
+}
